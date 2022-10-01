363f0055cbaa5d26ff6acf2f0942bc76be7844ea
Nikita Shpikin <nikita.shpikin@gmail.com>
2022-10-01 19:36:36 +0500

    реализовал вывод значения в input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Count from './components/Count';
 import './styles/app.sass';
 
 function App() {
+  const [priceValue, setPriceValue] = useState(1000000);
+  const [feeValue, setFeeValue] = useState(0);
+  const [termValue, setTermValue] = useState(0);
+  const [error, setError] = useState(false);
+
+  const getPrice = (e, min, max, setValue) => {
+    if (e >= min && e <= max) {
+      setError(false);
+      setValue(e);
+    } else if (isNaN(e)) {
+      setError(true);
+    } else {
+      setValue(e);
+      setError(true);
+    }
+  };
+
   return (
     <div className='container'>
       <h1 className='title'>Рассчитайте стоимость автомобиля в лизинг</h1>
-      <Count />
+      <div className='counts'>
+        <Count
+          text='Стоимость автомобиля'
+          errorValue='Введите стоимость автомобиля от 1 до 6 миллионов'
+          max={6_000_000}
+          min={1_000_000}
+          value={priceValue}
+          setValue={setPriceValue}
+          error={error}
+          setError={setError}
+          func={getPrice}
+        />
+        <Count
+          text='Первоначальный взнос'
+          errorValue='ошибка'
+          max={60}
+          min={10}
+          value={feeValue}
+          setValue={setFeeValue}
+          error={error}
+          setError={setError}
+          func={getPrice}
+        />
+        <Count
+          text='Срок лизинга'
+          errorValue='ошибка'
+          max={60}
+          min={1}
+          value={termValue}
+          setValue={setTermValue}
+          error={error}
+          setError={setError}
+          func={getPrice}
+        />
+      </div>
+
       <div className='result'>
         <div>
           <span>Сумма договора лизинга</span>
--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,69 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-export default function Count() {
-  const [priceValue, setPriceValue] = useState(0);
-  const [feeValue, setFeeValue] = useState(0);
-  const [termValue, setTermValue] = useState(0);
-
+export default function Count({
+  text,
+  value,
+  setValue,
+  min,
+  max,
+  errorValue,
+  error,
+  func
+}) {
   return (
-    <div className='count'>
-      <div>
-        <span className='count__subTitle'>Стоимость автомобиля</span>
-        <input className='count__input' type='text' />
-        <input
-          style={{
-            background: `linear-gradient(to right, #ff9800 0%, #ff9800 ${Math.floor(
-              ((priceValue - 1000000) / (6000000 - 1000000)) * 100
-            )}%, #E1E1E1 ${
-              Math.floor((priceValue - 1000000) / (6000000 - 1000000)) * 100
-            }%, #E1E1E1 100%)`
-          }}
-          defaultValue={priceValue}
-          className='count__range'
-          type='range'
-          min={1_000_000}
-          max={6_000_000}
-          onChange={e => setPriceValue(+e.target.value)}
-        />
-      </div>
-      <div>
-        <span className='count__subTitle'>Первоначальный взнос</span>
-        <input className='count__input' type='text' />
-        <input
-          style={{
-            background: `linear-gradient(to right, #ff9800 0%, #ff9800 ${Math.floor(
-              ((feeValue - 10) / (60 - 10)) * 100
-            )}%, #E1E1E1 ${
-              Math.floor((feeValue - 10) / (60 - 10)) * 100
-            }%, #E1E1E1 100%)`
-          }}
-          defaultValue={feeValue}
-          className='count__range'
-          type='range'
-          min={10}
-          max={60}
-          onChange={e => setFeeValue(+e.target.value)}
-        />
-      </div>
-      <div>
-        <span className='count__subTitle'>Срок лизинга</span>
-        <input className='count__input' type='text' />
-        <input
-          style={{
-            background: `linear-gradient(to right, #ff9800 0%, #ff9800 ${Math.floor(
-              ((termValue - 1) / (60 - 1)) * 100
-            )}%, #E1E1E1 ${
-              Math.floor((termValue - 1) / (60 - 1)) * 100
-            }%, #E1E1E1 100%)`
-          }}
-          defaultValue={termValue}
-          className='count__range'
-          type='range'
-          min={1}
-          max={60}
-          onChange={e => setTermValue(+e.target.value)}
-        />
-      </div>
+    <div>
+      <span className='counts__subTitle'>{text}</span>
+
+      <input
+        className='counts__input'
+        type='text'
+        value={value}
+        onChange={e => func(+e.target.value, min, max, setValue)}
+      />
+      {error && <span className='counts__error'>{errorValue}</span>}
+      <input
+        style={{
+          background: `linear-gradient(to right, #ff9800 0%, #ff9800 ${Math.floor(
+            ((value - min) / (max - min)) * 100
+          )}%, #E1E1E1 ${
+            Math.floor((value - min) / (max - min)) * 100
+          }%, #E1E1E1 100%)`
+        }}
+        defaultValue={value}
+        className='counts__range'
+        type='range'
+        min={min}
+        max={max}
+        onChange={e => setValue(+e.target.value)}
+        onInput={e => func(+e.target.value, min, max, setValue)}
+      />
     </div>
   );
 }
