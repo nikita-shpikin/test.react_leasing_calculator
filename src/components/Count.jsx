2bef854f255272301c1ce309c09020c81cf57e2a
Nikita Shpikin <nikita.shpikin@gmail.com>
2022-10-01 05:44:16 +0500

    написал логику вычисления процентов ползунка

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,36 +1,48 @@
 import React, { useState } from 'react';
 
 export default function Count() {
-  const [priceValue, setPriceValue] = useState(1_000_000);
-
-  const getPriceValue = e => {
-    console.log('x');
-    setPriceValue(+e.target.value);
-  };
+  const [priceValue, setPriceValue] = useState(0);
+  const [firstValue, setFirstValue] = useState(0);
 
   return (
     <div className='count'>
       <div>
         <span className='count__subTitle'>Стоимость автомобиля</span>
-        <input className='count__input' type='text' onChange={getPriceValue} />
-        <progress
-          className='count__range'
-          min={1_000_000}
-          max={6_000_000}
-        ></progress>
-        {/* <input
+        <input className='count__input' type='text' />
+        <input
+          style={{
+            background: `linear-gradient(to right, #ff9800 0%, #ff9800 ${Math.floor(
+              ((priceValue - 1000000) / (6000000 - 1000000)) * 100
+            )}%, #E1E1E1 ${
+              Math.floor((priceValue - 1000000) / (6000000 - 1000000)) * 100
+            }%, #E1E1E1 100%)`
+          }}
+          defaultValue={priceValue}
           className='count__range'
           type='range'
           min={1_000_000}
           max={6_000_000}
-          onInput={getPriceValue}
-          onClick={}
-        /> */}
+          onChange={e => setPriceValue(+e.target.value)}
+        />
       </div>
       <div>
         <span className='count__subTitle'>Первоначальный взнос</span>
         <input className='count__input' type='text' />
-        <input className='count__range' type='range' min={10} max={60} />
+        <input
+          style={{
+            background: `linear-gradient(to right, #ff9800 0%, #ff9800 ${Math.floor(
+              ((firstValue - 10) / (60 - 10)) * 100
+            )}%, #E1E1E1 ${
+              Math.floor((firstValue - 10) / (60 - 10)) * 100
+            }%, #E1E1E1 100%)`
+          }}
+          defaultValue={firstValue}
+          className='count__range'
+          type='range'
+          min={10}
+          max={60}
+          onChange={e => setFirstValue(+e.target.value)}
+        />
       </div>
       <div>
         <span className='count__subTitle'>Срок лизинга</span>
