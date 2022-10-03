cf57461100989dd4277dd76b8a059bc37dab327e
Nikita Shpikin <nikita.shpikin@gmail.com>
2022-10-03 18:31:36 +0500

    Configure React app for deployment to GitHub Pages

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 export default function Form({ monthPay, sumPay }) {
+  const data = { monthPay: monthPay, sumPay: sumPay };
+
   const submitHandler = e => {
     fetch('https://eoj3r7f3r4ef6v4.m.pipedream.net', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ monthPay: monthPay, sumPay: sumPay })
+      body: JSON.stringify(data)
     });
     e.event.preventDefault();
   };
